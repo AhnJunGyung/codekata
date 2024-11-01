c6b1d7d34c058dedd454f6b8400582595fa78e48
AJG <84224280+AhnJunGyung@users.noreply.github.com>
2024-11-01 09:40:36 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var result = 0
+    var num = n
+
+    while num > 0 {
+        result += num % 10
+        num /= 10    
+    }
+    
+    return result
+}
