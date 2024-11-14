dfedec245176ea0f4ff28f9afa434f02f500d608
AJG <84224280+AhnJunGyung@users.noreply.github.com>
2024-11-14 10:58:13 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 9.96 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
@@ -1,11 +1,3 @@
 func solution(_ n:Int) -> String {
-    let su = "수"
-    let park = "박"
-    var answer = ""
-    
-    for i in 1...n {
-        answer += i % 2 == 0 ? park : su
-    }
-    
-    return answer
+    return (1...n).map({ $0 % 2 == 0 ? "박" : "수" }).reduce("", + )
 }
