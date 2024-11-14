945673b33d3b6d338a96379758570f6be3800bb8
AJG <84224280+AhnJunGyung@users.noreply.github.com>
2024-11-14 11:58:01 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 13.05 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
@@ -1,3 +1,10 @@
 func solution(_ n:Int) -> String {
-    return (1...n).map({ $0 % 2 == 0 ? "박" : "수" }).reduce("", + )
+    return (1...n).lazy.map({ $0 % 2 == 0 ? "박" : "수" }).reduce("", + )
 }
+
+// func solution(_ s:String) -> String {
+//     let array: [Character] = Array(s)
+//     let middleCount = array.count / 2
+        
+//     return array.count % 2 == 0 ? String(array[middleCount - 1]) + String(array[middleCount]) : String(array[middleCount])
+// }
