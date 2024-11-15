0b0f3218a641c3c183b256be6f90944676626a3c
AJG <84224280+AhnJunGyung@users.noreply.github.com>
2024-11-15 10:06:18 +0900

    [level 1] Title: 내적, Time: 0.40 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.swift
+++ 프로그래머스/1/70128. 내적/내적.swift
@@ -1,11 +1,14 @@
 import Foundation
 
 func solution(_ a:[Int], _ b:[Int]) -> Int {
-    var result = 0
+//     var result = 0
     
-    for i in 0..<a.count {
-        result += a[i] * b[i]
-    }
+//     for i in 0..<a.count {
+//         result += a[i] * b[i]
+//     }
     
-    return result
+    // return result
+    //return a.map{ $0 * (b.map { $0 }) }.reduce(0)
+    
+    return (0..<a.count).map{a[$0] * b[$0]}.reduce(0, +)
 }
