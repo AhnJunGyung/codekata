ae3368d86c897cd68f3e27b6561ceca194c08766
AJG <84224280+AhnJunGyung@users.noreply.github.com>
2024-12-06 21:07:43 +0900

    [level 3] Title: 이중우선순위큐, Time: 3888.33 ms, Memory: 21 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
@@ -0,0 +1,64 @@
+import Foundation
+
+func solution(_ operations:[String]) -> [Int] {
+
+    var result: [Int] = []
+
+    for operation in operations {
+        
+        for value in operation {
+            var op = operation
+            
+            if value == "I" {
+                op.removeFirst()
+                op.removeFirst()
+                
+                if let number = Int(op) {
+                    result.append(number)
+                }
+                
+            } else if value == "D"{
+                op.removeFirst()
+                op.removeFirst()
+                
+                if result.count > 0 {
+                    if let number = Int(op) {
+                    // result가 비어있지 않은 경우
+                    // -1일 경우 최소값 삭제 / 1일 경우 최대값 삭제
+                    if number == -1 {
+                        if let minimumValue = result.min() {
+                            if let minimumIndex = result.firstIndex(of: minimumValue) {
+                                result.remove(at: minimumIndex)
+                            }
+                        }
+                    } else if number == 1 {
+                        if let maximumValue = result.max() {
+                            if let maximumIndex = result.firstIndex(of: maximumValue) {
+                                result.remove(at: maximumIndex)
+                            }
+                        }
+                    }
+                }
+                    
+                }
+                
+                
+            } 
+            
+        }
+        
+    
+    }
+    
+    if result.count == 0 {
+        result = [0, 0]
+    } else {
+        if let max = result.max() {
+            if let min = result.min() {
+                result = [max, min]
+            }
+        }
+    }
+    
+    return result
+}
