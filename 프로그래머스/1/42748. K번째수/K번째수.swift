e34f0fda1d9e1b3ac6047b09bce1e3d699ab9e18
AJG <84224280+AhnJunGyung@users.noreply.github.com>
2024-12-09 10:06:30 +0900

    [level 1] Title: K번째수, Time: 0.16 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -7,15 +7,10 @@ func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
     //배열을 i~j 까지 자른 후 sort로 오름차순 정렬
     //정렬된 배열에서 k번째 요소 result배열에 append
     
-    for i in 0..<commands.count {
-        var temporaryArray: [Int] = []
-
-        for j in commands[i][0] - 1...commands[i][1] - 1 {
-            temporaryArray.append(array[j])
-        }
-        
+    for i in commands {
+        var temporaryArray = Array(array[i[0]-1...i[1]-1])
         temporaryArray.sort()
-        result.append(temporaryArray[commands[i][2] - 1])
+        result.append(temporaryArray[i[2] - 1])
     }
     
     return result
