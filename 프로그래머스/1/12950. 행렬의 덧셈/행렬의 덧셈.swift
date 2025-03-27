223a0f75aea93968e2d320bdaf2c71a82592cb42
AJG <84224280+AhnJunGyung@users.noreply.github.com>
2025-03-27 12:14:05 +0900

    [level 1] Title: 행렬의 덧셈, Time: 13.44 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
@@ -0,0 +1,17 @@
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    
+    var result: [[Int]] = []
+    
+    for i in 0..<arr1.count {
+        
+        var array: [Int] = []
+        
+        for j in 0..<arr1[i].count {
+            array.append(arr1[i][j] + arr2[i][j])
+        }
+        
+        result.append(array)
+    }
+    
+    return result
+}
