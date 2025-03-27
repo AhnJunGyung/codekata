fc6458c13845f5dba9216631092b44fe6f36dc30
AJG <84224280+AhnJunGyung@users.noreply.github.com>
2025-03-27 14:09:26 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -1,35 +1,16 @@
 import Foundation
 
-func solution(_ n:Int) -> Int {
+func solution(_ n: Int) -> Int {
     
     var num = n
     var str = ""
-    var result = 0
 
-    // 3진법
+    // 3진법 변환
     while num > 0 {
         str += String(num % 3)
-        num = num / 3
+        num /= 3
     }
-    
-    // 뒤집기
-    str.reversed()
-    
-    num = Int(str)!
-    
-    // 10진법
-    var count = 1
-    
-    if num % 10 != 0 {
-        result += num % 10
-        num = num / 10
-    }
-    
-    while num > 0 {
-        result += (num % 10) * Int(pow(Double(3), Double(count)))
-        num = num / 10
-        count += 1
-    }
-    
-    return result
+
+    // 뒤집힌 3진법 값을 10진법으로 변환
+    return Int(str, radix: 3)!
 }
