82ec9303c53940bdc70c55577e5fd71dfc0d14bd
AJG <84224280+AhnJunGyung@users.noreply.github.com>
2025-04-27 14:57:03 +0900

    [level 1] Title: 기사단원의 무기, Time: 322.21 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.swift
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.swift
@@ -1,33 +1,23 @@
 import Foundation
 
 func solution(_ number: Int, _ limit: Int, _ power: Int) -> Int {
-    
-    /*
-    1. 1~number 까지 약수의 개수(= 공격력) 구하기
-    2. limit에 넘는 경우 power로 교체
-    */
-    
     var result = 0
     
     for num in 1...number {
-        
         var count = 0
-        let sqrtNum = Int(Double(num).squareRoot()) //제곱근 구하기
+        let sqrtNum = Int(Double(num).squareRoot())
 
-        for i in 1...sqrtNum {// 약수 계산
-            
+        for i in 1...sqrtNum {
             if num % i == 0 {
                 count += (i * i == num) ? 1 : 2
             }
-            
-            if count > limit { 
+            if count > limit {
                 count = power
-                break 
+                break
             }
         }
         
         result += count
-        
     }
     
     return result
